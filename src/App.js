79cdd75475ec01caceb40d692e39ab662d7b9606
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-09-03 16:48:43 -0700

    Add router. Add packages for redux.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/clients/1')
+      .then(resp => resp.json())
+      .then(data => console.log(data.data.attributes.bookings[0].day))
+  }
+  
   render () {
     return (
       <div className="App">
