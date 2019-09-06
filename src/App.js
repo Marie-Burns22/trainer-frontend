caf44c3d3764e2dd3141d4111aa10ddf042f1d0a
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-09-06 13:16:20 -0700

    Removed problem files and code. Created container components.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
-import LoginForm from './components/LoginForm';
-// import {connect} from 'react-redux'
 
+import BookingsContainer from './containers/BookingsContainer';
+import ServicesContainer from './containers/ServicesContainer';
+import ClientsContainer from './containers/ClientsContainer';
 
 class App extends React.Component {
 
@@ -13,53 +14,21 @@ class App extends React.Component {
       .then(data => console.log(data))
   }
   
-
-
   render () {
     return (
       <div className="App">
           <p>
             Welcome to Marie's Personal Trainer App
           </p>
-          <LoginForm />
+          <ClientsContainer />
+          <ServicesContainer />
+          <BookingsContainer />
         
       </div>
     );
   }
 }
 
-// const mapStateToProps = (state) => {
-//   return {
-//     bookings: 
-//   }
-// }
-
-// export default connect(mapStateToProps)(App);
 export default App;
 
 
-//From react auth using session video:
-
-// componentDidMount() {
-//   const token = localStorage.getItem("token")
-//   if (token) {
-//     fetch("http://localhost:3000/get_current_user", {
-//       credentials: "include",
-//       headers: {
-//         "Content-Type": "application/json"
-//       }
-//     })
-//     .then(r => r.json())
-//     .then(resp => {
-//       if (resp.error) {
-//         alert(resp.error)
-//       } else {
-//             this.setState({
-//               currentClient: resp.client
-//           })
-//         }
-//       }
-//     })
-//     .catch(console.log)
-//   }
-// }
