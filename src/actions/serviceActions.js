ebb319e7c2a399dbb174e38be6569933bb7ab934
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-09-06 14:53:37 -0700

    Fix fetch request bugs in reducer and action for services.

--- src/actions/serviceActions.js
+++ src/actions/serviceActions.js
@@ -4,7 +4,7 @@
 
 export default function fetchServices() {
     return (dispatch) => {
-        fetch('http://localhost3000/api/v1/services')
+        fetch('http://localhost:3000/api/v1/services')
         .then(resp => resp.json())
         .then(services =>  dispatch({
             type: "FETCH_SERVICES",
--- src/reducers/serviceReducer.js
+++ src/reducers/serviceReducer.js
@@ -1,11 +1,12 @@
 export default function serviceReducer(state = {services:[]}, action) {
     switch (action.type) {
         case "FETCH_SERVICES":
-            return state
+            return {services: action.payload}
 
     //     case "ADD_SERVICE":
     //         return action.service
         default:
             return state
     }
+
 }
