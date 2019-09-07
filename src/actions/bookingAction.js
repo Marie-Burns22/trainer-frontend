83dfdbbbb07096d39cc8f724ae3288b59dd6f669
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-09-07 11:24:35 -0700

    Add synchronous and async actions for bookings

--- src/actions/bookingAction.js
+++ src/actions/bookingAction.js
@@ -1,6 +1,29 @@
-export const setBooking = booking => {
+// synchronous actions
+export const setBookings = bookings => {
     return {
-        type: "ADD_BOOKING",
-        booking
+        type: "FETCH_BOOKINGS",
+        bookings
+    }
+}
+
+// async actions
+export const fetchBookings = () => {
+    return dispatch => {
+        return fetch("http:localhost:3000/api/v1/bookings", {
+            method: 'GET',
+            headers: {
+                "Content-Type": "applicatin/json"
+            },
+        })
+            .then(r => r.json())
+            .then(response => {
+                if (response.error) {
+                    alert(response.error)
+                } else {
+                    console.log(response.data)
+                    dispatch(setBookings(response.data))
+                }
+            })
+            .catch(console.log)
     }
 }
