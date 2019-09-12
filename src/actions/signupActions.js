724bd65a2d35f0d7a95c60689e78527e2656dcfe
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-09-12 14:32:58 -0700

    Add signup link and logic to navbar

--- src/actions/signupActions.js
+++ src/actions/signupActions.js
@@ -1,4 +1,15 @@
 // synchronous action creators
-// export const updateSignupForm
+export const updateSignupForm = formData => {
+    return {
+        type: "UPDATE_SIGNUP_FORM",
+        formData
+    }
+}
+
+export const resetSignupForm = () => {
+    return {
+        type: "RESET_SIGNUP_FORM"
+    }
+}
 
 // async action creators
--- src/navComponents/NavBar.js
+++ src/navComponents/NavBar.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import {NavLink} from 'react-router-dom'
 import Logout from '../clientComponents/Logout';
-import LoginForm from '../clientComponents/LoginForm'
+// import LoginForm from '../clientComponents/LoginForm'
 
 const NavBar = ({currentClient}) => {
     return (
         <div className="NavBar container">
-            {currentClient ? <p>Welcome, {currentClient.attributes.name}</p> : <p>Please Login or Signup</p>}
-            {currentClient ? <Logout /> : <LoginForm />}
+            {currentClient ? <p>Welcome, {currentClient.attributes.name}  <Logout /> </p> : <p>Please <NavLink to="/login">Login</NavLink>  or <NavLink to="/signup">Sign Up</NavLink></p>}
+            {/* {currentClient ? : <LoginForm />} */}
+            
         </div>
     )
 }
