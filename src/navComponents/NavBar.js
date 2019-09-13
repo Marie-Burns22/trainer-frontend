43e64e54651a061438c1e1df4463f851bf395f1d
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-09-13 14:14:56 -0700

    Add styling to navbar

--- src/navComponents/NavBar.js
+++ src/navComponents/NavBar.js
@@ -1,27 +1,71 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import {NavLink} from 'react-router-dom'
+// import {NavLink} from 'react-router-dom'
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Nav from 'react-bootstrap/Nav';
+import styled from 'styled-components';
+// import Form from 'react-bootstrap/Form';
+// import FormControl from 'react-bootstrap/FormControl';
+// import Button from 'react-bootstrap/Button'
+
+const Styles = styled.div`
+    .navbar {
+        background-color: #222;
+    }
+
+    .navbar-brand, .navbar-nav .nav.link {
+        color: #bbb;
+
+        &:hover {
+            color: white;
+        }
+    }
+`;
 
 const NavBar = ({currentClient}) => {
     return (
-        <Navbar bg="light" expand="lg">
-            {currentClient 
-            ? 
-            <NavLink to="/logout">Logout</NavLink> 
-            : 
-            <div>
-                <NavLink to="/login">Login</NavLink>
-                <NavLink to="/signup">Sign Up</NavLink> 
-            </div>
-            }
-            <NavLink to="/services">All Services</NavLink>
-        </Navbar>
-
-
-
-
-
+        // <Navbar bg="light" expand="lg">
+        //     {currentClient 
+        //     ? 
+        //     <NavLink to="/logout">Logout</NavLink> 
+        //     : 
+        //     <div>
+        //         <NavLink to="/login">Login</NavLink>
+        //         <NavLink to="/signup">Sign Up</NavLink> 
+        //     </div>
+        //     }
+        //     <NavLink to="/services">All Services</NavLink>
+        // </Navbar>
+        <Styles>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="/">Vegas in Miami</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        {currentClient ?
+                        <Nav.Item><Nav.Link href="/logout">Logout</Nav.Link></Nav.Item>
+                        :
+                        <React.Fragment>
+                            <Nav.Item><Nav.Link href="/login">Login</Nav.Link></Nav.Item>
+                            <Nav.Item><Nav.Link href="/signup">Sign Up</Nav.Link></Nav.Item>
+                        </React.Fragment>    
+                        }
+                        <NavDropdown title="Services" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/services">All Services</NavDropdown.Item>
+                            <NavDropdown.Item href="/services/new">New Service</NavDropdown.Item>
+                            {/* <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item> */}
+                            {/* <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
+                        </NavDropdown>
+                    </Nav>
+                    {/* <Form inline>
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                        <Button variant="outline-success">Search</Button>
+                    </Form> */}
+                </Navbar.Collapse>
+            </Navbar>
+        </Styles>
     )
 }
 
